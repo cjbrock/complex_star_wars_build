7c08fa0f807c50e20bb2f93fa5deee3d26fca928
cjbrock <corinna@flatironschool.com>
2017-12-07 11:02:46 -0500

    correct iteration through the json categories

--- lib/good_full_star_wars/categories.rb
+++ lib/good_full_star_wars/categories.rb
@@ -2,12 +2,10 @@ class GoodFullStarWars::Categories
   attr_accessor :category, :category_url
 
   def self.new_cats_from_api(cats)
-    cats.each_with_index do |key, value|
       self.new(
-        key,
-        value
+        cats[0],
+        cats[1]
       )
-    end
   end
 
   def initialize(cat=nil, cat_url=nil)
